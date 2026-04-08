ce100916842b05f9d8518e9b29005e051a1f223a
Nathan A Molyneux <100804742+NathanAMolyneux@users.noreply.github.com>
2026-04-07 19:16:25 -0500

    WIP animation, ships, factory
    Not completed work, but in progress player animations, moving platforms ( state), moving ships (factory)

---
+++ CIS 414 3D Platformer/Assets/Scripts/NMScriots/NMPlayerInteraction.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NMPlayerInteraction : MonoBehaviour
+{
+
+    [SerializeField] private float interactDistance = 3f;
+    [SerializeField] private LayerMask interactLayer;
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TryInteract();
+        }
+    }
+
+    private void TryInteract()
+    {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactDistance, interactLayer))
+        {
+            CloseDoorPanel panel = hit.collider.GetComponentInParent<CloseDoorPanel>();
+            if (panel != null)
+            {
+                panel.Interact();
+                return;
+            }
+
+            Door door = hit.collider.GetComponentInParent<Door>();
+            if (door != null)
+            {
+                door.ToggleDoor();
+                return;
+            }
+        }
+    }
+}
