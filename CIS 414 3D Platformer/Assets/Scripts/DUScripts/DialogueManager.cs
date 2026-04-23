e4f212416c9b57f7c8b53b019ebe38ae27f6df33
Dominique Uzuegbu <Dominique.uzuegbu@yahoo.com>
2026-04-23 18:46:13 -0500

    Level Design Stuff

---
+++ CIS 414 3D Platformer/Assets/Scripts/DUScripts/DialogueManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private static DialogueManager instance = null;
+    public UnityEvent uEvent;
+    public GameObject TriggerObject;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject == TriggerObject)
+        {
+            Debug.Log("You Got Level One Clearance!");
+
+        }
+
+    }
+
+    public static DialogueManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new DialogueManager();
+            }
+            return instance;
+        }
+    }
+}
