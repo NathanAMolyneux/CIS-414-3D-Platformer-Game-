d94517847885f4878b89b789cebe79d272e50459
Dominique Uzuegbu <Dominique.uzuegbu@yahoo.com>
2026-04-30 17:23:26 -0500

    Decorator & Level Updates

---
+++ CIS 414 3D Platformer/Assets/Scripts/DUScripts/ClearanceLevel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearanceLevel : IDecorator
+{
+    public int getClearance()
+    {
+        return 0;
+    }
+}
---
+++ CIS 414 3D Platformer/Assets/Scripts/DUScripts/HandleRotation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleRotation : MonoBehaviour
+{
+    
+/*
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.rotation = Quaternion.transform(0, 0, -90);
+        }
+
+    }
+*/
+}
